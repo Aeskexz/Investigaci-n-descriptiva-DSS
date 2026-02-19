0f28a7e63120122adde6dc670f870ddcb34074d7
Gabo <gabo.avila20@gmail.com>
2026-02-19 11:42:15 -0600

    se hizo la validacion si un doctor ya tiene cita en la misma fecha y hora con validaciones

--- src/services/citasService.js
+++ src/services/citasService.js
@@ -1,4 +1,6 @@
-exports.validarDisponibilidad = (datosCita) => {
+const db = require('../db');
+// se le agrega async 
+exports.validarDisponibilidad = async (datosCita) => {
 
     const { fecha, hora } = datosCita;
 
@@ -32,9 +34,31 @@ exports.validarDisponibilidad = (datosCita) => {
     // 1. Consultar la base de datos
     // 2. Verificar si el doctor ya tiene cita en esa fecha y hora
     // 3. Retornar disponible: false si está ocupado
+    try {
+        // se busca si existe cita para el mismo doctor fecha y hora, se usa await para que js 
+        // espere la respuesta de la bd
+        const query = 'SELECT * FROM citas WHERE fecha = ? AND hora = ? AND doctor_id = ?';
+        const [rows] = await db.query(query, [fecha, hora, doctor_id]);
 
-    return {
-        disponible: true,
-        mensaje: "Horario válido"
-    };
+        // verificacion si el doctor ya tiene cita
+        if (rows.length > 0) {
+            return {
+                disponible: false,
+                mensaje: "El doctor ya tiene una cita programada para esa fecha y hora."
+            };
+        }
+
+        // si sale este mensaje todo bien
+        return {
+            disponible: true,
+            mensaje: "Horario disponible"
+        };
+
+    } catch (error) {
+        console.error("Error al consultar la disponibilidad:", error);
+        return {
+            disponible: false,
+            mensaje: "Error interno al validar la disponibilidad."
+        };
+    }
 };
