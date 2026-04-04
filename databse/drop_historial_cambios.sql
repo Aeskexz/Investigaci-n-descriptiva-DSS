b5506c1dcbb73af5d985475a7dfcbd13281c3fe8
Donetsk1 <ivan.castillo03@outlook.com>
2026-04-04 16:29:59 -0600

    correcciones

--- databse/drop_historial_cambios.sql
+++
@@ -1,8 +0,0 @@
--- Elimina la tabla historial_cambios solo si existe
--- Ejecuta este script en la base de datos investigacion_descriptiva
-
-USE investigacion_descriptiva;
-
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS historial_cambios;
-SET FOREIGN_KEY_CHECKS = 1;
